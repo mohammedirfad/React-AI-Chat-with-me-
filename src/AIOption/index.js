dd6902055e8c105bd181fde4f8bb03d7c2b14504
Mohammed irfad <muhammedirfad43@gmail.com>
2023-07-10 15:37:26 +0530

    first commit

---
+++ src/AIOption/index.js
@@ -0,0 +1,52 @@
+export const arrayItems = [{
+    name:"Q&A",
+    id :"q&a",
+    description:"Answer the question with Existing Knowledge",
+    option:{
+        model: "text-davinci-003",
+        temperature: 0,
+        max_tokens: 100,
+        top_p: 1,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+        stop: ["\n"],
+    }
+
+},
+{
+    name:"Grammer Correction",
+    id :"grammerCorrection",
+    description:"Correct the grammer"
+},
+{
+    name:"Grammer Correction",
+    id :"grammerCorrection",
+    description:"Correct the grammer"
+},
+{
+    name:"Grammer Correction",
+    id :"grammerCorrection",
+    description:"Correct the grammer"
+}
+,
+{
+    name:"Grammer Correction",
+    id :"grammerCorrection",
+    description:"Correct the grammer"
+},
+{
+    name:"Grammer Correction",
+    id :"grammerCorrection",
+    description:"Correct the grammer"
+},
+{
+    name:"Grammer Correction",
+    id :"grammerCorrection",
+    description:"Correct the grammer"
+},
+{    name:"Q&A",
+id :"q&a",
+description:"Answer the question with Existing Knowledge"
+}
+
+]
--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Configuration, OpenAIApi } from "openai";
+
+
+import { arrayItems } from "./AIOption";
+import { useState } from "react";
+import Translation from "./Components/Transilation";
+import OptionSelection from "./Components/OptionSelection";
 
 function App() {
+  const configuration = new Configuration({
+    apiKey:"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",
+  });
+  const openai = new OpenAIApi(configuration);
+  const [option, setOption] = useState({});
+  const [result, setResult] = useState("");
+  const [input, setInput] = useState("");
+  // console.log(import.meta.env.VITE_Open_AI_Key);
+  const selectOption = (option) => {
+    setOption(option);
+  };
+
+  // const doStuff = async () => {
+  //   let object = { ...option, prompt:input };
+
+  //   const response = await openai.createCompletion(object);
+
+  //   setResult(response.data.choices[0].text);
+  // };
+  // console.log(option);
+  const doStuff = async () => {
+    let object = { ...option, prompt: input };
+
+    await callOpenAI(object);
+  };
+
+  const callOpenAI = async (object, numRetries = 3, delayMs = 1000) => {
+    try {
+      const response = await openai.createCompletion(object);
+      setResult(response.data.choices[0].text);
+    } catch (error) {
+      if (error.response && error.response.status === 429 && numRetries > 0) {
+        console.log('Rate limit exceeded. Retrying...');
+        await delay(delayMs);
+        await callOpenAI(object, numRetries - 1, delayMs * 2);
+      } else {
+        console.error('Error:', error);
+      }
+    }
+  };
+
+  const delay = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Object.values(option).length === 0 ? (
+        <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
+      ) : (
+        <Translation doStuff={doStuff} setInput={setInput} result={result}/>
+      )}
     </div>
   );
 }
